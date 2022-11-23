c232e5d1fa84e13a9fc1ec6e4a2405c940527503
Hussein <aisackhussein@gmail.com>
2022-11-23 22:05:45 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Muktar Hussein on 23/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
